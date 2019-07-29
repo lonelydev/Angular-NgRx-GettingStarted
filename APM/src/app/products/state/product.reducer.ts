9395c9bb76c391557ae622a9009a4281a68f160c
Eakan Gopalakrishnan <egopalakrishnan@cfcunderwriting.com>
2019-07-29 14:55:31 +0100

    Define selectors for the ProductState. Learn the difference between:   * createFeatureSelector   * createSelector

--- APM/src/app/products/state/product.reducer.ts
+++ APM/src/app/products/state/product.reducer.ts
@@ -1,10 +1,10 @@
 import { Product } from '../product';
 import * as fromRoot from '../../state/app.state';
+import { createFeatureSelector, createSelector } from '@ngrx/store';
 
 export interface State extends fromRoot.State {
   products: ProductState;
 }
-
 
 export interface ProductState {
   showProductCode: boolean;
@@ -17,6 +17,27 @@ const initialState: ProductState = {
   currentProduct: null,
   products: []
 };
+
+/**
+ * There is no forward referencing in typescript, so declare constants in the order of usage.
+ * If one has to be used later, declare it first.
+ */
+const getProductFeatureState = createFeatureSelector<ProductState>('products');
+
+export const getShowProductCode = createSelector(
+  getProductFeatureState,
+  state => state.showProductCode
+);
+
+export const getCurrentProduct = createSelector(
+  getProductFeatureState,
+  state => state.currentProduct
+);
+
+export const getProducts = createSelector(
+  getProductFeatureState,
+  state => state.products
+);
 
 /**
  * Reducers generally takes two parameters
