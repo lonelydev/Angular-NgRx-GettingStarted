6251923432c5a219d8e5498a167dff70201a88a2
Eakan Gopalakrishnan <egopalakrishnan@cfcunderwriting.com>
2019-08-01 23:16:10 +0100

    Update effect. Using it in the product edit component.

--- APM/src/app/products/state/product.effects.ts
+++ APM/src/app/products/state/product.effects.ts
@@ -4,7 +4,8 @@ import { ProductService } from '../product.service';
 import * as productActions from './product.actions';
 import { mergeMap, map, catchError } from 'rxjs/operators';
 import { Product } from '../product';
-import { of } from 'rxjs';
+import { of, Observable } from 'rxjs';
+import { Action } from '@ngrx/store';
 
 @Injectable()
 export class ProductEffects {
@@ -14,13 +15,28 @@ export class ProductEffects {
   ) {}
 
   @Effect()
-  loadProducts$ = this.actions$.pipe(
+  loadProducts$: Observable<Action> = this.actions$.pipe(
     ofType(productActions.ProductActionTypes.Load),
     mergeMap((action: productActions.Load) =>
       this.productService.getProducts().pipe(
         map((products: Product[]) => new productActions.LoadSuccess(products)),
         // catchError does not automatically create an observable so use the of operator
         catchError(err => of(new productActions.LoadFail(err)))
+      )
+    )
+  );
+
+  @Effect()
+  updateProducts$: Observable<Action> = this.actions$.pipe(
+    ofType(productActions.ProductActionTypes.UpdateProduct),
+    map((action: productActions.UpdateProduct) => action.payload),
+    mergeMap((product: Product) =>
+      this.productService.updateProduct(product).pipe(
+        map(
+          updatedProduct =>
+            new productActions.UpdateProductSuccess(updatedProduct)
+        ),
+        catchError(err => of(new productActions.UpdateProductFail(err)))
       )
     )
   );
