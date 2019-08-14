c21689f39cd18bed5cfc3c559a722ff6f57c44aa
Eakan Gopalakrishnan <egopalakrishnan@cfcunderwriting.com>
2019-08-14 15:39:02 +0100

    Introducing Barrels To reduce import noise, we barrel all exports from multiple files in a folder into one file called index.ts.
    One file to export them all.

---
+++ APM/src/app/products/state/index.ts
@@ -0,0 +1,53 @@
+import * as fromRoot from '../../state/app.state';
+import { createFeatureSelector, createSelector } from '@ngrx/store';
+import { ProductState } from './product.reducer';
+
+export interface State extends fromRoot.State {
+  products: ProductState;
+}
+
+/**
+ * There is no forward referencing in typescript, so declare constants in the order of usage.
+ * If one has to be used later, declare it first.
+ */
+const getProductFeatureState = createFeatureSelector<ProductState>('products');
+
+export const getShowProductCode = createSelector(
+  getProductFeatureState,
+  state => state.showProductCode
+);
+
+export const getCurrentProductId = createSelector(
+  getProductFeatureState,
+  state => state.currentProductId
+);
+
+export const getCurrentProduct = createSelector(
+  getProductFeatureState,
+  getCurrentProductId,
+  (state, currentProductId) => {
+    if (currentProductId === 0) {
+      return {
+        id: 0,
+        productName: '',
+        productCode: 'New',
+        description: '',
+        starRating: 0
+      };
+    } else {
+      return currentProductId
+        ? state.products.find(p => p.id === currentProductId)
+        : null;
+    }
+  }
+);
+
+export const getProducts = createSelector(
+  getProductFeatureState,
+  state => state.products
+);
+
+export const getError = createSelector(
+  getProductFeatureState,
+  state => state.error
+);
--- APM/src/app/products/state/product.reducer.ts
+++ APM/src/app/products/state/product.reducer.ts
@@ -1,12 +1,5 @@
 import { Product } from '../product';
-import * as fromRoot from '../../state/app.state';
-import { createFeatureSelector, createSelector } from '@ngrx/store';
 import { ProductActionTypes, ProductActions } from './product.actions';
-import { ProductListComponent } from '../components/product-list/product-list.component';
-
-export interface State extends fromRoot.State {
-  products: ProductState;
-}
 
 export interface ProductState {
   showProductCode: boolean;
@@ -21,52 +14,6 @@ const initialState: ProductState = {
   products: [],
   error: ''
 };
-
-/**
- * There is no forward referencing in typescript, so declare constants in the order of usage.
- * If one has to be used later, declare it first.
- */
-const getProductFeatureState = createFeatureSelector<ProductState>('products');
-
-export const getShowProductCode = createSelector(
-  getProductFeatureState,
-  state => state.showProductCode
-);
-
-export const getCurrentProductId = createSelector(
-  getProductFeatureState,
-  state => state.currentProductId
-);
-
-export const getCurrentProduct = createSelector(
-  getProductFeatureState,
-  getCurrentProductId,
-  (state, currentProductId) => {
-    if (currentProductId === 0) {
-      return {
-        id: 0,
-        productName: '',
-        productCode: 'New',
-        description: '',
-        starRating: 0
-      };
-    } else {
-      return currentProductId
-        ? state.products.find(p => p.id === currentProductId)
-        : null;
-    }
-  }
-);
-
-export const getProducts = createSelector(
-  getProductFeatureState,
-  state => state.products
-);
-
-export const getError = createSelector(
-  getProductFeatureState,
-  state => state.error
-);
 
 /**
  * Reducers generally takes two parameters
